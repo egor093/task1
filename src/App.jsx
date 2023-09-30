b74c97077536287fbadcb0d194f299945c98466a
egor093 <egoriksib@gmail.com>
2023-09-30 15:49:05 +0300

    a

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './style/App.css';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Main from './Components/Mian/Main';
+
+function App() {
+    return (
+        <div className="container">
+
+            <Header />
+            <Main />
+            <Footer />
+
+        </div>
+    );
+}
+
+export default App;
